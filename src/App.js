d71c311afdf560bfe68d683185679c8bd6efb437
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-03-10 02:49:48 +0530

    add commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
+  // Navigate,
 } from "react-router-dom";
-import { getAuth } from "firebase/auth";
+// import { getAuth } from "firebase/auth";
 import Home from "./components/Home/Home";
 import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
-import { onAuthStateChanged } from "firebase/auth";
+// import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in
-        setUser(user);
-      } else {
-        // No user is signed in
-        setUser(null);
-      }
-    });
-    return unsubscribe;
-  }, []);
+  // useEffect(() => {
+  //   const auth = getAuth();
+  //   const unsubscribe = onAuthStateChanged(auth, (user) => {
+  //     if (user) {
+  //       // User is signed in
+  //       setUser(user);
+  //     } else {
+  //       // No user is signed in
+  //       setUser(null);
+  //     }
+  //   });
+  //   return unsubscribe;
+  // }, []);
 
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={user ? <Navigate to="/signup" /> : " "} />
+          <Route path="/" element={<Signup />} />
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
